df55bd7c53406c9a09e34d5b491eb6235d1f42ec
Robert Brightline <robert.brightline@gmail.com>
2025-03-24 21:16:00 -0500

    feat: add Apollo Server sample with GraphQL subscriptions and user management

--- samples/nestjs-graphql/src/main.ts
+++ samples/nestjs-graphql/src/main.ts
@@ -1,29 +1,63 @@
 import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 import { Logger, Module } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
-import { GraphQLModule } from '@nestjs/graphql';
+import { Args, GraphQLModule, Mutation, Subscription } from '@nestjs/graphql';
 import {
   FastifyAdapter,
   NestFastifyApplication,
 } from '@nestjs/platform-fastify';
 import { Resolver, Query } from '@nestjs/graphql';
 import { ApolloServerPluginLandingPageLocalDefault } from '@apollo/server/plugin/landingPage/default';
+import { PubSub } from 'graphql-subscriptions';
+
+const pubSub = new PubSub();
 
 @Resolver(() => String)
-export class ExampleResolver {
+export class HelloResolver {
   @Query(() => String)
   hello(): string {
     return 'Hello World!';
   }
 }
 
+type User = Record<string, any>;
+
+@Resolver('User')
 export class UserResolver {
-  @Query(() => String)
-  user() {
-    return {
-      username: 'John Doe',
-      email: '[email protected]',
+  private users: User[] = [];
+
+  @Query('getUser')
+  getUser(@Args('id') id: string): User | undefined {
+    return this.users.find((user) => user.id === id);
+  }
+
+  @Query('getAllUsers')
+  getAllUsers(): User[] {
+    return this.users;
+  }
+
+  @Mutation('createUser')
+  async createUser(@Args('input') input: any): Promise<User> {
+    const newUser = {
+      id: Date.now().toString(),
+      username: input.username,
     };
+    this.users.push(newUser);
+
+    await pubSub.publish('userAdded', { userAdded: newUser });
+    return newUser;
+  }
+
+  @Mutation('deleteUser')
+  deleteUser(@Args('id') id: string): boolean {
+    const initialLength = this.users.length;
+    this.users = this.users.filter((user) => user.id !== id);
+    return this.users.length < initialLength;
+  }
+
+  @Subscription('userAdded')
+  userAdded() {
+    return pubSub.asyncIterableIterator('userAdded');
   }
 }
 
@@ -35,9 +69,12 @@ export class UserResolver {
       playground: false,
       sortSchema: true,
       plugins: [ApolloServerPluginLandingPageLocalDefault()],
+      subscriptions: {
+        'graphql-ws': true,
+      },
     }),
   ],
-  providers: [ExampleResolver],
+  providers: [HelloResolver, UserResolver],
 })
 class AppModule {}
 
